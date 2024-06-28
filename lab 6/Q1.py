10f8cf52f3c434aa57f55363e4895aeeb8b68eae
Annur21-alt <nuraidanabila08@gmail.com>
2024-06-29 03:02:12 +0800

    update

--- lab 6/Q1.py
+++ lab 6/Q1.py
@@ -1,5 +1,5 @@
 #Write a python function that takes a number as parameter and prints the multiplication table of that number
-
+'''
 # using def function
 def multiplication_table(number):
     for i in range(1,11):
@@ -7,4 +7,17 @@ def multiplication_table(number):
 
 #instruction to user
 number = int(input("Please enter the number: "))
+multiplication_table(number)
+'''
+
+print ("This program will takes a number as parameter and prints the multiplication table of that number")
+
+#instruction to user
+number = int(input("Please enter the number:"))
+
+# using def function
+def multiplication_table(number):
+    for i in range (1,13):
+        print(f"{number} x {i} ={number * i}")
+
 multiplication_table(number)
